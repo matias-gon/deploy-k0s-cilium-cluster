ff0e5c9e5c996f49d3ad02cdd72705af8e2b860e
Matias Gon <matiasgon@chaco.gob.ar>
2026-06-17 12:10:38 -0300

    fixin relative file path and k0sctl comman name

--- deploy-k0s-cilium-cluster.sh
+++ deploy-k0s-cilium-cluster.sh
@@ -6,13 +6,16 @@ cat ~/.ssh/cilium-k0s.pub | xargs -I{} echo "      - {}" >> cloud-init.yaml
 
 # creating the cluster nodes with multipass
 export NETWORK=$(multipass networks | grep ethernet | awk '{print $1}')
-multipass launch -n cp01 -c 1 -m 1G -d 5G --network $NETWORK --cloud-init /home/matias/Documents/lab/k0s-cluster/cloud-init.yaml
-multipass launch -n worker01 -c 4 -m 3G -d 10G --network $NETWORK --cloud-init /home/matias/Documents/lab/k0s-cluster/cloud-init.yaml
-multipass launch -n worker02 -c 4 -m 3G -d 10G --network $NETWORK --cloud-init /home/matias/Documents/lab/k0s-cluster/cloud-init.yaml
+multipass launch -n cp01 -c 1 -m 1G -d 5G --network $NETWORK --cloud-init ./cloud-init.yaml
+multipass launch -n worker01 -c 6 -m 2G -d 10G --network $NETWORK --cloud-init ./cloud-init.yaml
+multipass launch -n worker02 -c 6 -m 2G -d 10G --network $NETWORK --cloud-init ./cloud-init.yaml
+
+# waiting for the nodes to be up and running
+wait 30
 
 # applying the cilium cluster configuration
 cp cilium-cluster-template.yaml cilium-cluster.yaml
 multipass info cp01 --format json | jq .info.cp01.ipv4[1] | xargs -I{} sed -i 's/<CP01Address>/{}/g' cilium-cluster.yaml
 multipass info worker01 --format json | jq .info.worker01.ipv4[1] | xargs -I{} sed -i 's/<Worker01Address>/{}/g' cilium-cluster.yaml
 multipass info worker02 --format json | jq .info.worker02.ipv4[1] | xargs -I{} sed -i 's/<Worker02Address>/{}/g' cilium-cluster.yaml
-kosctl apply --config cilium-cluster.yaml
+k0sctl apply --config cilium-cluster.yaml
